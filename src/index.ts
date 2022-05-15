c218ae957a477c3e7a7b37151bc9c0f41aabc7d3
Ignacio <ij.dotta@gmail.com>
2022-05-15 20:42:05 -0300

    patch for BigInt toJSON

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,28 @@
-import express, {Request, Response, NextFunction} from "express";
+import express, { Request, Response, NextFunction } from "express";
+import { PrismaClient, patients } from "@prisma/client";
+require('~/patch.js')
 
 let port = process.env.PORT || 3000;
 var app = express();
+const prisma = new PrismaClient();
 
-app.get('/', (req: Request, res: Response, next: NextFunction) => {
-    res.send('Hello world!');
-})
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+    res.send("Hello world!");
+});
+
+app.get("/patients", handlePatientsRequest );
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
 });
+
+async function handlePatientsRequest(req: Request, res: Response, next: NextFunction) {
+    let patients = await getPatients();
+    res.send(patients);
+}
+
+async function getPatients() : Promise<Array<patients>> {
+    const patients = await prisma.patients.findMany();
+    console.log(patients);
+    return patients;
+}
