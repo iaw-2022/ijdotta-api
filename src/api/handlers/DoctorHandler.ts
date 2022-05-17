8bcaa551397ae5365172c5d226b5c1a4d275f13e
Ignacio <ij.dotta@gmail.com>
2022-05-16 22:06:46 -0300

    doctors api

---
+++ src/api/handlers/DoctorHandler.ts
@@ -0,0 +1,39 @@
+import { NextFunction, Request, Response } from "express";
+import apiUtils from '~/api/utils';
+import doctorController from "~/controller/DoctorController";
+import { DoctorAppointmentsRequestType } from "~/types/doctor";
+
+class DoctorHandler {
+
+    async getDoctors(req: Request, res: Response, next: NextFunction) {
+        let result;
+
+        try {
+            result = await doctorController.getDoctors();
+        } catch (error) {
+            next(error);
+            return false;
+        }
+
+        apiUtils.sendResponse(res, result);
+    }
+
+    async getAppointments(req: Request, res: Response, next: NextFunction) {
+        let result;
+
+        try {
+            const doctor: DoctorAppointmentsRequestType = {
+                id: BigInt(req.params.id),
+            }
+
+            result = await doctorController.getAppointments(doctor);
+        } catch (error) {
+            next(error);
+            return false;
+        }
+
+        apiUtils.sendResponse(res, result);
+    }
+}
+
+export default new DoctorHandler();
--- src/api/routes.ts
+++ src/api/routes.ts
@@ -1,19 +1,31 @@
 import { Router } from "express";
 import ROUTES from "~/constants/routes";
 import appointmentHandler from "./handlers/AppointmentHandler";
+import doctorHandler from "./handlers/DoctorHandler";
 import patientHandler from "./handlers/PatientHandler";
 
 const api = Router();
 
+/**
+ * Appointments routes
+ */
 api.route(ROUTES.APPOINTMENTS.BY_ID)
     .put(appointmentHandler.bookAppointment)
     .delete(appointmentHandler.cancelAppointment);
 
 api.get(ROUTES.APPOINTMENTS.ALL, appointmentHandler.findAll);
 
+/**
+ * Patients routes
+ */
 api.get(ROUTES.PATIENT.BY_ID, patientHandler.getProfile);
 api.get(ROUTES.PATIENT.APPOINTMENTS.ALL, patientHandler.getAppointments);
 api.get(ROUTES.PATIENT.TREATMENTS.ALL, patientHandler.getTreatments);
 
+/**
+ * Doctors routes
+ */
+api.get(ROUTES.DOCTOR.ALL, doctorHandler.getDoctors);
+api.get(ROUTES.DOCTOR.APPOINTMENTS, doctorHandler.getAppointments);
 
 export default api;
