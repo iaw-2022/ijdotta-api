6cb1a31dccd58c2447f9dbc21194c3a8b9bad985
Ignacio <ij.dotta@gmail.com>
2022-05-20 12:08:09 -0300

    swagger-jsdocs documentation

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -7,12 +7,53 @@ import patientHandler from "./handlers/PatientHandler";
 const api = Router();
 
 /**
- * Appointments routes
+ * @swagger
+ * /appointments/{id}:
+ *  put:
+ *      description: book an appointment
+ *  delete:
+ *      description: cancel an appointment
+ *  parameters:
+ *      - in: path
+ *        name: id
+ *        required: true
+ *        schema:
+ *          type: integer
+ *          minimum: 1
+ *        description: the appointment ID
+ *      - in: body
+ *        name: patient_id
+ *        description: the patient to assign the appointment
+ *        schema:
+ *          type: object
+ *          required:
+ *            - patient_id
+ *          properties:
+ *            patient_id:
+ *              type: integer
  */
 api.route(ROUTES.APPOINTMENTS.BY_ID)
     .put(appointmentHandler.bookAppointment)
     .delete(appointmentHandler.cancelAppointment);
 
+/**
+ * @swagger
+ * /appointments:
+ *   get:
+ *     description: returns all the appointments
+ *   parameters:
+ *     - in: query
+ *       name: patient_id
+ *       schema:
+ *         type: integer
+ *         minimum: 1
+ *   responses:
+ *     '200':
+ *       description: a list of appointments
+ *       content:
+ *         schema:
+ *           type: object
+ */
 api.get(ROUTES.APPOINTMENTS.ALL, appointmentHandler.findAll);
 
 /**
