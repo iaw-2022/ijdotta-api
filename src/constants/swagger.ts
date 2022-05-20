171377d9806f21e29de64115f97044f56dd51330
Ignacio <ij.dotta@gmail.com>
2022-05-20 12:07:45 -0300

    swagger constants

---
+++ src/constants/swagger.ts
@@ -0,0 +1,9 @@
+const SWAGGER = {
+    API_NAME: 'ClinicApp API',
+    DESCRIPTION: 'API to access patients profile and book/cancel appointments at a clinic.',
+    CONTACT: {
+        NAME: 'Ignacio Joaquín Dotta'
+    }
+}
+
+export default SWAGGER;
