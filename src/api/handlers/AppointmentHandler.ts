dd064e870c5042d37e4d20cf70047fb0453666f6
Ignacio <ij.dotta@gmail.com>
2022-05-27 14:48:08 -0300

    auto-formatted

--- src/api/handlers/AppointmentHandler.ts
+++ src/api/handlers/AppointmentHandler.ts
@@ -2,7 +2,11 @@ import { Request, Response, NextFunction } from 'express';
 import { AppointmentRequestType, AppointmentSearchRequestType } from '~/types/appointment';
 import appointmentController from '~/controller/AppointmentController';
 import apiUtils from '~/api/utils';
-import { parseAppointmentSearchRequest, parseAppointmentRequestOnlyParams, parseAppointmentRequestWithBody } from './parser';
+import {
+  parseAppointmentSearchRequest,
+  parseAppointmentRequestOnlyParams,
+  parseAppointmentRequestWithBody,
+} from './parser';
 class AppointmentHandler {
   async findAll(req: Request, res: Response, next: NextFunction) {
     let result;
