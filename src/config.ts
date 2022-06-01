ec96bbb9c97d6b7df5531c82b96a57dceb95fa33
Ignacio <ij.dotta@gmail.com>
2022-06-01 17:56:34 -0300

    moved port logic to config file

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,14 @@
 const CONFIG = {
+  
+    SERVER: {
+        PORT: process.env.PORT || 3000,
+    },
 
-  AUTH0: {
-    AUDIENCE: process.env.AUDIENCE,
-    ISSUER_BASE_URL: process.env.ISSUER_BASE_URL,
-    EMAIL_NAMESPACE: process.env.AUDIENCE + 'email',
-  }
-
-}
+    AUTH0: {
+        AUDIENCE: process.env.AUDIENCE,
+        ISSUER_BASE_URL: process.env.ISSUER_BASE_URL,
+        EMAIL_NAMESPACE: process.env.AUDIENCE + 'email',
+    },
+};
 
 export default CONFIG;
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,20 @@
 import express, { Request, Response, NextFunction } from 'express';
+import CONFIG from './config';
+import { noop } from 'lodash';
 require('~/patch.js');
 import api from '~/api/index';
 
-let port = process.env.PORT || 3000;
 var app = express();
 
 app.use('/api', api);
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  noop(req)
+  noop(next)
   res.send('Hello world!');
 });
 
+const port = CONFIG.SERVER.PORT;
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
